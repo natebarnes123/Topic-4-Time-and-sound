8acca8b8062a41b08dd0c87cc9768c8495086939
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-29 09:02:04 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,6 +9,15 @@ namespace Topic_4_Time_and_sound
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Rectangle bombRect;
+        Texture2D bombTexture;
+        Texture2D pliers;
+        SpriteFont timeFont;
+        SoundEffect explode;
+        float seconds;
+        MouseState mouseState;
+
 
         public Game1()
         {
@@ -19,23 +29,42 @@ namespace Topic_4_Time_and_sound
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            bombRect = new Rectangle(50, 50, 700, 400);
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+            seconds = 0f;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            bombTexture = Content.Load<Texture2D>("bomb");
+            timeFont = Content.Load<SpriteFont>("TimeFont");
+            bombRect = new Rectangle(50, 50, 700, 400);
 
+            explode = Content.Load<SoundEffect>("explosion");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (seconds > 10)
+            {
+                seconds = 0f;
+                explode.Play();
+            }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+
+            
 
             base.Update(gameTime);
         }
@@ -45,7 +74,11 @@ namespace Topic_4_Time_and_sound
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timeFont, (10 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
